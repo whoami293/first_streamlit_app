ed1371fa752c536c9eac481ca9ef685b9274d1d7
whoami293 <130478881+whoami293@users.noreply.github.com>
2023-04-12 00:01:30 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 
 import streamlit
+import pandas
 
 streamlit.title('My Parents New Healthy Diner')
 # streamlit.header('Breakfast Menu') COPY THESE:    🥑🍞
@@ -8,3 +9,6 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(my_fruit_list)
