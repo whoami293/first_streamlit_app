447ac0cf17d78360e90caec8a8008b6e305aaaa5
whoami293 <130478881+whoami293@users.noreply.github.com>
2023-04-12 19:34:42 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ except URLError as e:
   streamlit.error()
 
 
-streamlit.header("The fruit load list contains")
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list")
